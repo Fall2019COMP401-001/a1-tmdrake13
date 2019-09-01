2c6bd25cab20487e2c423d63e967ae1a8c6756fa
UNC Support <UNC Support@UNC-R90RDGU3.wireless-psk.unc.edu>
2019-09-01 13:39:55 -0400

    improved commenting and style

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -5,96 +5,109 @@ import java.util.Scanner;
 public class A1Adept {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scan = new Scanner(System.in);
 
-		//input number of items in store
+		// input number of items in store
 		int numstoreitems = scan.nextInt();
-		
-		Item [] storeitems = new Item [numstoreitems];
-		
+
+		Item[] storeitems = new Item[numstoreitems];
+
 		for (int i = 0; i < numstoreitems; i++) {
-			//input name of item
+			// input name of item
 			String name = scan.next();
-			//input price of item
+			// input price of item
 			double price = scan.nextDouble();
-			storeitems[i] = new Item (name, price);
-			
+			storeitems[i] = new Item(name, price);
+
 		}
-		
-		//input number of customers
+
+		// input number of customers
 		int numcustomers = scan.nextInt();
-		 Customer [] customers = new Customer [numcustomers];
-		
+		Customer[] customers = new Customer[numcustomers];
+
 		for (int i = 0; i < numcustomers; i++) {
-			//input first name of customer
+			// input first name of customer
 			String fname = scan.next();
-			//input last name of customer
+			// input last name of customer
 			String lname = scan.next();
-			//input number of items bought
+			// input number of items bought
 			int numitemsbought = scan.nextInt();
 			
-			customers[i] = new Customer (fname, lname, numitemsbought);
-	
-			
+			//add new customer to array of customers based on inputed parameters
+			customers[i] = new Customer(fname, lname, numitemsbought);
+
 			for (int j = 0; j < customers[i].numitemsbought; j++) {
-				//input integer quantity
-				 int frequency = scan.nextInt();
-				 //input name of items bought
-				 String name = scan.next();
+				// input integer quantity
+				int frequency = scan.nextInt();
+				// input name of items bought
+				String name = scan.next();
+
+				double price = 0;
+
+				//this loop matches up the inputed item bought with the original
+				//list of items based on the items name. This is necessary in order
+				//to calculate the price of that item
 				
-				 double price = 0;
-				 
-					 
 				for (int m = 0; m < storeitems.length; m++) {
 					if (storeitems[m].name.equals(name)) {
 						price = storeitems[m].price;
 					}
 				}
-				 
-				 customers[i].itemsbought[j] = new Item (frequency, name, price);
-
 				
-				customers[i].setTotalSpent (customers[i].itemsbought[j].price * customers[i].itemsbought[j].frequency);
+				//once the price of the item is determined, that item, along with its price,
+				//is added to the array of items bought for each customer
+				customers[i].itemsbought[j] = new Item(frequency, name, price);
+
+				//using the array of items bought for each customer, we calculate
+				//the total amount spent by that customer and use the method setTotalSpent
+				//to store that value as an instance variable for that customer
+				customers[i].setTotalSpent(customers[i].itemsbought[j].price * customers[i].itemsbought[j].frequency);
 			}
-		}		
-		
-			
-		
+		}
+
 		scan.close();
 		
-			double max = 0;
-			String maxname = "";
-			for (int i = 0; i < customers.length; i++) {
-				if (customers[i].totalspent > max) {
-					max = customers [i].totalspent;
-					maxname = customers[i].firstname + " " + customers[i].lastname;
-				}
-			
-		}
-			
-			double min = Double.MAX_VALUE;
-			String minname = "";
-			for (int i = 0; i < customers.length; i++) {
-				if (customers[i].totalspent < min) {
-					min = customers [i].totalspent;
-					minname = customers[i].firstname + " " +  customers[i].lastname;
-				}
-			
-		}
-			
-			double average;
-			double sum = 0;
-			for (int i = 0; i < customers.length; i++) {
-				sum += customers[i].totalspent;
+		//now that the total amount spent has been calcualted and stored for
+		//each customer, we then determine the max/min amount spent by using
+		//a simple algorithm
+
+		double max = 0;
+		String maxname = "";
+		for (int i = 0; i < customers.length; i++) {
+			if (customers[i].totalspent > max) {
+				max = customers[i].totalspent;
+				maxname = customers[i].firstname + " " + customers[i].lastname;
 			}
-			average = sum / customers.length ;
-			
-			System.out.println ("Biggest: " + maxname + " (" + String.format("%.2f", max) + ")");
-			System.out.println ("Smallest: " + minname + " (" + String.format("%.2f", min) + ")");
-			System.out.println ("Average: " + String.format("%.2f", average));
-			
+
+		}
+
+		double min = Double.MAX_VALUE;
+		String minname = "";
+		for (int i = 0; i < customers.length; i++) {
+			if (customers[i].totalspent < min) {
+				min = customers[i].totalspent;
+				minname = customers[i].firstname + " " + customers[i].lastname;
+			}
+
+		}
 		
+		//finally, we calculate the average amount spent for all customers
+
+		double average;
+		double sum = 0;
+		for (int i = 0; i < customers.length; i++) {
+			sum += customers[i].totalspent;
+		}
+		average = sum / customers.length;
+
+		//we then print off the max, min, and average values we just calculated
+		//using the correct two-decimal format
+		
+		System.out.println("Biggest: " + maxname + " (" + String.format("%.2f", max) + ")");
+		System.out.println("Smallest: " + minname + " (" + String.format("%.2f", min) + ")");
+		System.out.println("Average: " + String.format("%.2f", average));
+
 	}
-	
+
 }
--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -5,98 +5,102 @@ import java.util.Scanner;
 public class A1Jedi {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scan = new Scanner(System.in);
 
-		//input number of items in store
-				int numstoreitems = scan.nextInt();
-				
-				Item [] storeitems = new Item [numstoreitems];
-				
-				for (int i = 0; i < numstoreitems; i++) {
-					//input name of item
-					String name = scan.next();
-					//input price of item
-					double price = scan.nextDouble();
-					storeitems[i] = new Item (name, price);
-					
-				}
-				
-				//input number of customers
-				int numcustomers = scan.nextInt();
-				 Customer [] customers = new Customer [numcustomers];
-				
-				for (int i = 0; i < numcustomers; i++) {
-					//input first name of customer
-					String fname = scan.next();
-					//input last name of customer
-					String lname = scan.next();
-					//input number of items bought
-					int numitemsbought = scan.nextInt();
-					
-					customers[i] = new Customer (fname, lname, numitemsbought);
-			
-					
-					for (int j = 0; j < customers[i].numitemsbought; j++) {
-						//input integer quantity
-						 int frequency = scan.nextInt();
-						 //input name of items bought
-						 String name = scan.next();
-						
-						 customers[i].itemsbought[j] = new Item (frequency, name);
+		// input number of items in store
+		int numstoreitems = scan.nextInt();
 
-						customers[i].setTotalSpent (customers[i].itemsbought[j].price * customers[i].itemsbought[j].frequency);
-					}
-				}		
-				
-					
-				
-				scan.close();
-				
-				 for (int i = 0; i < storeitems.length; i ++) {
-					 for (int j = 0; j < customers.length; j++) {
-						 for (int m = 0; m < customers[j].itemsbought.length; m++) {
-							 if (customers[j].itemsbought[m].name.equals((storeitems[i].name))) {
-								 storeitems[i].incCustBought();
-								 break;
-							 }
-									 
-						 }
-					 }
-					 
-				 } 
-				 
-				 //a similar loop is ran twice in order to increase customer count and
-				 //total number bought - this avoids a bug in which multiple items of the 
-				 //same kind are listed separately for each customer
-				 
-				 for (int i = 0; i < storeitems.length; i ++) {
-					 for (int j = 0; j < customers.length; j++) {
-						 for (int m = 0; m < customers[j].itemsbought.length; m++) {
-							 if (customers[j].itemsbought[m].name.equals((storeitems[i].name))) {
-								 storeitems[i].addTotalNumBought(customers[j].itemsbought[m].frequency);
-							 }
-									 
-						 }
-					 }
-					 
-				 }
-				 
-				
-				 
-				 
-				 
-				 for (int i = 0; i < storeitems.length; i++ ) {
-					 if (storeitems[i].custbought != 0) {
-					 System.out.println (storeitems[i].custbought + " customers bought " + storeitems[i].totalnumbought + " " +  storeitems[i].name); }
-					 else {
-						 System.out.println ("No customers bought " + storeitems[i].name);
-					 }
-					 
-				 }
-				
-	}
-	
-	
+		Item[] storeitems = new Item[numstoreitems];
+
+		for (int i = 0; i < numstoreitems; i++) {
+			// input name of item
+			String name = scan.next();
+			// input price of item
+			double price = scan.nextDouble();
+			storeitems[i] = new Item(name, price);
+
+		}
+
+		// input number of customers
+		int numcustomers = scan.nextInt();
+		Customer[] customers = new Customer[numcustomers];
+
+		for (int i = 0; i < numcustomers; i++) {
+			// input first name of customer
+			String fname = scan.next();
+			// input last name of customer
+			String lname = scan.next();
+			// input number of items bought
+			int numitemsbought = scan.nextInt();
+
+			//add new customer to array of customers based on inputed parameters
+			customers[i] = new Customer(fname, lname, numitemsbought);
+
+			for (int j = 0; j < customers[i].numitemsbought; j++) {
+				// input integer quantity
+				int frequency = scan.nextInt();
+				// input name of items bought
+				String name = scan.next();
+
+				//each item bought is added to the array of items bought
+				//for that customer
+				customers[i].itemsbought[j] = new Item(frequency, name);
+
+			}
+		}
+
+		scan.close();
+
+		//this loop increases the count for times bought
+		//for each item if a customer buys it. The loop breaks
+		//for that customer if an item is found, which avoids a 
+		//bug that can cause the same customer to be counted twice
+		//if the orders are listed separately
 		
+		for (int i = 0; i < storeitems.length; i++) {
+			for (int j = 0; j < customers.length; j++) {
+				for (int m = 0; m < customers[j].itemsbought.length; m++) {
+					if (customers[j].itemsbought[m].name.equals((storeitems[i].name))) {
+						storeitems[i].incCustBought();
+						break;
+					}
+
+				}
+			}
+
+		}
+
+		// a similar loop is ran twice in order to increase customer count and
+		// total number bought - this avoids a bug in which multiple items of the
+		// same kind are listed separately for each customer
+
+		for (int i = 0; i < storeitems.length; i++) {
+			for (int j = 0; j < customers.length; j++) {
+				for (int m = 0; m < customers[j].itemsbought.length; m++) {
+					if (customers[j].itemsbought[m].name.equals((storeitems[i].name))) {
+						storeitems[i].addTotalNumBought(customers[j].itemsbought[m].frequency);
+					}
+
+				}
+			}
+
+		}
+		
+		//this loops prints the result for the number of customers who bought
+		//each item - if not customers bought that item, it prints
+		//a separate message, so a conditional if-else is used
+
+		for (int i = 0; i < storeitems.length; i++) {
+			if (storeitems[i].custbought != 0) {
+				System.out.println(storeitems[i].custbought + " customers bought " + storeitems[i].totalnumbought + " "
+						+ storeitems[i].name);
+			} else {
+				System.out.println("No customers bought " + storeitems[i].name);
+			}
+
+		}
+
+	}
+
 }
